d401dff593b579d72c92c71db8e9519994cc42cc
eren <eren.turkoglu@epitech.eu>
2025-08-03 20:26:12 +0200

    Added die() and msg() for error handling

--- src/client.cpp
+++ src/client.cpp
@@ -16,121 +16,18 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <string>
+#include <vector>
 
-const size_t k_max_msg = 4096;
-
-static void msg(const char *msg) {
+static void msg(const char *msg)
+{
     fprintf(stderr, "%s\n", msg);
 }
 
-static void die(const char *msg) {
+static void die(const char *msg)
+{
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
 }
 
-static int32_t read_full(int fd, char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = read(fd, buf, n);
-        if (rv <= 0) {
-            return -1;  // error, or unexpected EOF
-        }
-        assert((size_t)rv <= n);
-        n -= (size_t)rv; // to read
-        buf += rv; // move pointer
-    }
-    return 0;
-}
-
-static int32_t write_all(int fd, const char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = write(fd, buf, n);
-        if (rv <= 0) {
-            return -1;  // error
-        }
-        assert((size_t)rv <= n);
-        n -= (size_t)rv;
-        buf += rv;
-    }
-    return 0;
-}
-
-static int32_t query(int fd, const char *text) {
-    uint32_t len = (uint32_t)strlen(text);
-    if (len > k_max_msg) {
-        return -1;
-    }
-
-    char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);  // assume little endian
-    memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
-        return err;
-    }
-
-    // 4 bytes header
-    char rbuf[4 + k_max_msg + 1];
-    errno = 0;
-    int32_t err = read_full(fd, rbuf, 4);
-    if (err) {
-        msg(errno == 0 ? "EOF" : "read() error");
-        return err;
-    }
-
-    memcpy(&len, rbuf, 4);  // assume little endian
-    if (len > k_max_msg) {
-        msg("too long");
-        return -1;
-    }
-
-    // reply body
-    err = read_full(fd, &rbuf[4], len);
-    if (err) {
-        msg("read() error");
-        return err;
-    }
-
-    // do something
-    printf("server says: %.*s\n", len, &rbuf[4]);
-    return 0;
-}
-
-// TODOS: The protocol parsing code requires at least 2 read() syscalls per request. 
-// The number of syscalls can be reduced by using “buffered IO”. That is: 
-// read as much as you can into a
-// buffer at once, then try to parse multiple requests from that buffer
-
-int main() {
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        die("socket()");
-    }
-
-    struct sockaddr_in addr = {};
-    addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1:1234
-    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-    if (rv) {
-        die("connect");
-    }
-
-    // multiple requests
-    int32_t err = query(fd, "hello1");
-    if (err) {
-        close(fd);
-        return -1;
-    }
-    err = query(fd, "hello2");
-    if (err) {
-        close(fd);
-        return -1;
-    }
-    err = query(fd, "hello3");
-    if (err) {
-        close(fd);
-        return -1;
-    }
-    close(fd);
-    return 0;
-}
